f970386ec75d5bf393ab5783620cdd5b1da1ac7e
Zak <zakadmaster@gmail.com>
2020-01-24 10:35:38 +0100

    Addition of styled components

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+background: white;
+width: 33%;
+padding: 10px;
+margin: 10px auto;
+`;
 
 function StarWars(props) {
     console.log(props);
     return(
-        <div>
+        <Div>
             <h1>{props.name}</h1>
             <h3>Height: {props.height}</h3>
             <h3>Mass: {props.mass}</h3>
@@ -12,7 +20,7 @@ function StarWars(props) {
             <h3>Eye color: {props.eye_color}</h3>
             <h3>Birth: {props.birth_year}</h3>
             <h3>Gender: {props.gender}</h3>
-        </div>
+        </Div>
     )
 }
 
